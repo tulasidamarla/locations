b9fe4a8496431eb542b656f9246bd48036e02d90
tulasi <ram.damarla@gmail.com>
2017-10-07 21:20:14 +0530

    read truststore from classpath and accessed https url

--- locations/src/main/java/com/test/locations/LocationsApplication.java
+++ locations/src/main/java/com/test/locations/LocationsApplication.java
@@ -1,19 +1,28 @@
 package com.test.locations;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.security.KeyStore;
 import java.util.List;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
 
 @SpringBootApplication
 @RestController
@@ -29,6 +38,7 @@ public class LocationsApplication {
 		String baseUrl = "https://bookdrive-user-api.telstra.net/api/locationgetAll";
 
 		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl).queryParam("adminPass", "n1nz@");
+		System.out.println(builder.build().encode().toUri());
 		List<?> locations = restTemplate.getForObject(builder.build().encode().toUri(), List.class);
 		return locations;
 	}
@@ -43,25 +53,44 @@ public class LocationsApplication {
 		return locations;
 	}
 	
+	@Value("${keyStore}")
+	private String keyStoreName;
 	
-	/*public String securedLocation(){
-		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-		keyStore.load(new FileInputStream(new File("keystore.jks")),
-		        "secret".toCharArray());
-		SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
-		        new SSLContextBuilder()
-		                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
-		                .loadKeyMaterial(keyStore, "password".toCharArray()).build());
-		HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
-		ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
-		        httpClient);
-		RestTemplate restTemplate = new RestTemplate(requestFactory);
-		ResponseEntity<String> response = restTemplate.getForEntity(
-		        "https://localhost:8443", String.class);
-	}*/
+	@Value("${keyStorePassword}")
+	private String keyStorePassword;
 	
+	@Autowired
+    private ResourceLoader resourceLoader;
 	
-	@ResponseBody
+	@RequestMapping(value = "/cert/location", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<?> certlocation() {
+		List<?> locations = null;
+		try {
+			KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+
+			keyStore.load(resourceLoader.getResource("classpath:" + keyStoreName).getInputStream(),
+					keyStorePassword.toCharArray());
+
+			SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
+					new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build(),
+					NoopHostnameVerifier.INSTANCE);
+
+			HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+
+			ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+			RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+			String baseUrl = "https://bookdrive-user-api.telstra.net/api/locationgetAll";
+			UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl).queryParam("adminPass", "n1nz@");
+			locations = restTemplate.getForObject(builder.build().encode().toUri(), List.class);
+		} catch (Exception e) {
+			System.out.println("Exception" + e);
+		}
+		return locations;
+	}
+
+	
+/*	@ResponseBody
 	@RequestMapping(value = "/groups", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String groups() {
 		RestTemplate restTemplate = new RestTemplate();
@@ -72,6 +101,6 @@ public class LocationsApplication {
 		String response = restTemplate.getForObject(builder.build().encode().toUri(), String.class);
 		return response;
 
-	} 
+	} */
 
 }
